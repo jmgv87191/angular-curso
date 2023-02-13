33396cdf75ab179a321a7d939b0ad515146a6dab
jmgv87191 <jmgv87191@gmail.com>
2023-02-12 22:53:39 -0700

    ng-model

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre:'Trunks',
+    poder: 14000
+  }
+
+  agregar( ){
+    console.log(this.nuevo);
+  }
+
+
 
 }
